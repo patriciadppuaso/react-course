a8ec276cba2ca9ef09175e422e2703fbc6d0fba0
patriciadppuaso <patricia.puaso@getfullsuite.com>
2026-02-19 09:29:16 +0800

    Split header into a component and used Link to

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,36 +1,138 @@
-import './header.css';
-import './OrdersPage.css';
+import "./OrdersPage.css";
+import { Header } from "../components/Header";
 
 export function OrdersPage() {
   return (
     <>
-     <title>Orders</title>
-      <div className="header">
-        <div className="left-section">
-          <a href="/" className="header-link">
-            <img className="logo" src="images/logo-white.png" />
-            <img className="mobile-logo" src="images/mobile-logo-white.png" />
-          </a>
-        </div>
+      <title>Orders</title>
+      <Header />
 
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
+      <div class="orders-page">
+        <div class="page-title">Your Orders</div>
 
-          <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
-          </button>
-        </div>
+        <div class="orders-grid">
+          <div class="order-container">
+            <div class="order-header">
+              <div class="order-header-left-section">
+                <div class="order-date">
+                  <div class="order-header-label">Order Placed:</div>
+                  <div>August 12</div>
+                </div>
+                <div class="order-total">
+                  <div class="order-header-label">Total:</div>
+                  <div>$35.06</div>
+                </div>
+              </div>
 
-        <div className="right-section">
-          <a className="orders-link header-link" href="/orders">
-            <span className="orders-text">Orders</span>
-          </a>
+              <div class="order-header-right-section">
+                <div class="order-header-label">Order ID:</div>
+                <div>27cba69d-4c3d-4098-b42d-ac7fa62b7664</div>
+              </div>
+            </div>
 
-          <a className="cart-link header-link" href="/checkout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-          </a>
+            <div class="order-details-grid">
+              <div class="product-image-container">
+                <img src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+              </div>
+
+              <div class="product-details">
+                <div class="product-name">
+                  Black and Gray Athletic Cotton Socks - 6 Pairs
+                </div>
+                <div class="product-delivery-date">Arriving on: August 15</div>
+                <div class="product-quantity">Quantity: 1</div>
+                <button class="buy-again-button button-primary">
+                  <img
+                    class="buy-again-icon"
+                    src="images/icons/buy-again.png"
+                  />
+                  <span class="buy-again-message">Add to Cart</span>
+                </button>
+              </div>
+
+              <div class="product-actions">
+                <a href="tracking.html">
+                  <button class="track-package-button button-secondary">
+                    Track package
+                  </button>
+                </a>
+              </div>
+
+              <div class="product-image-container">
+                <img src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" />
+              </div>
+
+              <div class="product-details">
+                <div class="product-name">
+                  Adults Plain Cotton T-Shirt - 2 Pack
+                </div>
+                <div class="product-delivery-date">Arriving on: August 19</div>
+                <div class="product-quantity">Quantity: 2</div>
+                <button class="buy-again-button button-primary">
+                  <img
+                    class="buy-again-icon"
+                    src="images/icons/buy-again.png"
+                  />
+                  <span class="buy-again-message">Add to Cart</span>
+                </button>
+              </div>
+
+              <div class="product-actions">
+                <a href="tracking.html">
+                  <button class="track-package-button button-secondary">
+                    Track package
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="order-container">
+            <div class="order-header">
+              <div class="order-header-left-section">
+                <div class="order-date">
+                  <div class="order-header-label">Order Placed:</div>
+                  <div>June 10</div>
+                </div>
+                <div class="order-total">
+                  <div class="order-header-label">Total:</div>
+                  <div>$41.90</div>
+                </div>
+              </div>
+
+              <div class="order-header-right-section">
+                <div class="order-header-label">Order ID:</div>
+                <div>b6b6c212-d30e-4d4a-805d-90b52ce6b37d</div>
+              </div>
+            </div>
+
+            <div class="order-details-grid">
+              <div class="product-image-container">
+                <img src="images/products/intermediate-composite-basketball.jpg" />
+              </div>
+
+              <div class="product-details">
+                <div class="product-name">Intermediate Size Basketball</div>
+                <div class="product-delivery-date">Arriving on: June 17</div>
+                <div class="product-quantity">Quantity: 2</div>
+                <button class="buy-again-button button-primary">
+                  <img
+                    class="buy-again-icon"
+                    src="images/icons/buy-again.png"
+                  />
+                  <span class="buy-again-message">Add to Cart</span>
+                </button>
+              </div>
+
+              <div class="product-actions">
+                <a href="tracking.html">
+                  <button class="track-package-button button-secondary">
+                    Track package
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
