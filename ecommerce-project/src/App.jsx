aeb879a4ecff88ca7d918bf17606d340111112dd
patriciadppuaso <patricia.puaso@getfullsuite.com>
2026-02-19 13:28:30 +0800

    Lifted cart up and shared it between pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,18 +2,27 @@ import "./App.css";
 import { HomePage } from "./pages/HomePage";
 import { CheckoutPage } from "./pages/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
-import { TrackingPage } from './pages/TrackingPage';
-import { Routes, Route } from 'react-router';
+import { TrackingPage } from "./pages/TrackingPage";
+import { Routes, Route } from "react-router";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/cart-items").then((response) => {
+      setCart(response.data);
+    });
+  }, []);
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="/" element={<HomePage cart={cart}/>} />
+      <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
       <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />} />
-    </Routes>     
-  );    
+    </Routes>
+  );
 }
 
 export default App;
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,9 +4,9 @@ import { Header } from "../components/Header";
 
 import "./HomePage.css";
 
-export function HomePage() {
+export function HomePage({cart}) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
+  
 
   useEffect(() => {
     axios
@@ -15,11 +15,6 @@ export function HomePage() {
         setProducts(response.data);
       });
 
-    axios
-    .get("/api/cart-items")
-    .then((response) => {
-      setCart(response.data);
-    });
   }, []);
 
   return (
