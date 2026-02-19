c5cd887616ea191bbee128771de4257854af065f
patriciadppuaso <patricia.puaso@getfullsuite.com>
2026-02-19 08:54:43 +0800

    Moved tracking page into React

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,14 +2,16 @@ import "./App.css";
 import { HomePage } from "./pages/HomePage";
 import { CheckoutPage } from "./pages/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
-
+import { TrackingPage } from './pages/TrackingPage';
 import { Routes, Route } from 'react-router';
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>     
   );    
 }
